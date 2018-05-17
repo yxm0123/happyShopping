d99880da5c4139d371a3d3fab9b20dbac6d4dc9c
yxm0123 <771685455@qq.com>
2018-05-17 21:00:05 +0800

    首页完成

---
+++ js/common.js
@@ -0,0 +1,5 @@
+/**
+ * Created by yangxiaoman on 2018/5/17.
+ */
+
+var ip ="192.168.45.23";
---
+++ js/index.js
@@ -0,0 +1,71 @@
+/**
+ * Created by yangxiaoman on 2018/5/17.
+ */
+
+$(function () {
+
+
+
+  //1.渲染list页面
+
+  $.ajax({
+    dataType: 'json',
+    url: 'http://' + ip + ':9999/api/getindexmenu',
+    success: function (info) {
+
+      console.log(info);
+      var htmlStr = template("listTemp", info);
+      $('.main_list ul').html(htmlStr);
+
+    }
+  })
+
+
+  //2.点击更多 让隐藏的元素显示
+
+  $('.main_list ul').on('click','li:nth-child(8)',function(){
+    console.log(1);
+    $('li:nth-child(8)~li').slideToggle(200);
+  })
+
+
+  //3.渲染产品
+
+  $.ajax({
+    dataType:'json',
+    url:'http://' + ip + ':9999/api/getmoneyctrl',
+    success:function(info){
+      console.log(info);
+      var htmlStr = template("prodTemp",info);
+      $('.main_product .product_list').html(htmlStr)
+    }
+  })
+
+
+  //4.回到顶部
+  //$('.goTop').click(function(){
+  //  console.log(1);
+  //  $('html,body').animate({'scrollTop':0},1000)
+  //});
+
+  var goTop = document.getElementsByClassName('goTop')[0];
+
+  var id;
+
+  goTop.onclick=function(){
+    console.log(55);
+
+    id=setInterval(function(){
+
+      var current = document.documentElement.scrollTop;
+
+      if(current <=0){
+        clearInterval(id);
+        document.documentElement.scrollTop=0;
+      }
+      document.documentElement.scrollTop-=20;
+    },20)
+  }
+
+  //-----------------------
+})
